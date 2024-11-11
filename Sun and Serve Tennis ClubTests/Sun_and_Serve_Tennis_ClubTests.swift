3f8210b0176621e4ba394cef0a8f5b3f9b1e7ffc
Adrianna <125148770+nevatka@users.noreply.github.com>
2024-11-11 17:49:51 +0100

    Initial Commit

---
+++ Sun and Serve Tennis ClubTests/Sun_and_Serve_Tennis_ClubTests.swift
@@ -0,0 +1,17 @@
+//
+//  Sun_and_Serve_Tennis_ClubTests.swift
+//  Sun and Serve Tennis ClubTests
+//
+//  Created by Ada on 11/11/2024.
+//
+
+import Testing
+@testable import Sun_and_Serve_Tennis_Club
+
+struct Sun_and_Serve_Tennis_ClubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Sun and Serve Tennis ClubUITests/Sun_and_Serve_Tennis_ClubUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Sun_and_Serve_Tennis_ClubUITestsLaunchTests.swift
+//  Sun and Serve Tennis ClubUITests
+//
+//  Created by Ada on 11/11/2024.
+//
+
+import XCTest
+
+final class Sun_and_Serve_Tennis_ClubUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
